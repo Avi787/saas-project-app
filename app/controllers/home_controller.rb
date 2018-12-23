e42aff8aff92b8a5ff01a64cf364bc548968cd8a
Rails Udemy Course <railsudemycourse@gmail.com>
2018-12-23 18:26:09 -0500

    Add bootstrap styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
 
   def index
+    if current_user
+      if session[:tentant_id]
+        Tenant.set_current_tenant session[:tentant_id]
+      else
+        Tenant.set_current_tenant current_user.tentants.first
+      end
+      
+      @tenant = Tenant.current_tenant
+      params[:tenant_id] = @tenant.id
+    end
   end
 end
